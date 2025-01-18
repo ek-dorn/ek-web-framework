dbb37c30f312f5f4899e8a5140705a9df8985b9b
pseusys <aleksandr.sergeev.dm@gmail.com>
2025-01-18 21:05:18 +0100

    build fixed

--- example-website/src/app/app.routes.ts
+++ example-website/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PageComponent } from './page/page.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
+    { path: 'portfolio', component: PageComponent }
+];
